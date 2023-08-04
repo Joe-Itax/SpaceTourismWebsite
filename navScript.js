b976df5c86bf114b22ef788fd355cc21c4bda6f7
Joseph ITAKALA <josephitakala18@gmail.com>
2023-08-04 14:19:09 +0100

    feat : add an addEventListner to the page navScript

--- navScript.js
+++ navScript.js
@@ -1,15 +1,17 @@
 const iconHamburger = document.getElementById('iconHamburger');
-const navMobile = document.getElementById('navMobile')
-iconHamburger.onclick = () => {
-  if (iconHamburger.src.includes("icon-hamburger.svg")) {
-    navMobile.style.left = "initial";
-    navMobile.style.display = "inherit";
-    iconHamburger.src = "../assets/shared/icon-close.svg";
-    navMobile.style.transition = "left 0.5s ease-out, display 0.5s ease-out";
+const navMobile = document.getElementById('navMobile');
+displayMenuMobile = () => {
+  if (iconHamburger.src.includes('icon-hamburger.svg')) {
+    navMobile.style.left = 'initial';
+    navMobile.style.display = 'inherit';
+    iconHamburger.src = '../assets/shared/icon-close.svg';
+    navMobile.style.transition = 'left 0.5s ease-out, display 0.5s ease-out';
   } else {
-    navMobile.style.left = "-100%";
-    navMobile.style.display = "none";
-    iconHamburger.src = "../assets/shared/icon-hamburger.svg";
-    navMobile.style.transition = "left 0.5s ease-out, display 0.5s ease-out";
+    navMobile.style.left = '-100%';
+    navMobile.style.display = 'none';
+    iconHamburger.src = '../assets/shared/icon-hamburger.svg';
+    navMobile.style.transition = 'left 0.5s ease-out, display 0.5s ease-out';
   }
-}
+};
+
+iconHamburger.addEventListener('click', displayMenuMobile);
