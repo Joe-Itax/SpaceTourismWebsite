66c4e9a96194fc304fa917ef421553d23bd6f746
Joseph ITAKALA <josephitakala18@gmail.com>
2023-08-25 16:47:31 +0100

    minify all js and css files after updates

--- navScript.js
+++ navScript.js
@@ -1,17 +1,1 @@
-const iconHamburger = document.getElementById('iconHamburger');
-const navMobile = document.getElementById('navMobile');
-displayMenuMobile = () => {
-  if (iconHamburger.src.includes('icon-hamburger.svg')) {
-    navMobile.style.left = 'initial';
-    navMobile.style.display = 'inherit';
-    iconHamburger.src = '../assets/shared/icon-close.svg';
-    navMobile.style.transition = 'left 0.5s ease-out, display 0.5s ease-out';
-  } else {
-    navMobile.style.left = '-100%';
-    navMobile.style.display = 'none';
-    iconHamburger.src = '../assets/shared/icon-hamburger.svg';
-    navMobile.style.transition = 'left 0.5s ease-out, display 0.5s ease-out';
-  }
-};
-
-iconHamburger.addEventListener('click', displayMenuMobile);
+const iconHamburger=document.getElementById("iconHamburger"),navMobile=document.getElementById("navMobile");displayMenuMobile=(()=>{iconHamburger.src.includes("icon-hamburger.svg")?(navMobile.style.left="initial",navMobile.style.display="inherit",iconHamburger.src="../assets/shared/icon-close.svg",navMobile.style.transition="left 0.5s ease-out, display 0.5s ease-out"):(navMobile.style.left="-100%",navMobile.style.display="none",iconHamburger.src="../assets/shared/icon-hamburger.svg",navMobile.style.transition="left 0.5s ease-out, display 0.5s ease-out")}),iconHamburger.addEventListener("click",displayMenuMobile);
